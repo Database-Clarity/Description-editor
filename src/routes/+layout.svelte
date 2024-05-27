bbc01ce99b84beba7718ef474cb565a8959ad398
Icemourne <cboweris@gmail.com>
2024-05-27 21:12:16 +0300

    Added nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,9 @@ const { children } = $props()
 
 <header>
   <nav class="bg-neutral-800 mb-2">
-    <div>Nav bar</div>
-    <a href="/" target="_blank" rel="noopener noreferrer">Home</a>
-    <a href="/descriptionEditor/0" target="_self">Description Editor</a>
+    <div>Nav bar work in progress</div>
+    <a href="/">Home</a>
+    <a href="/descriptionEditor/">Description Editor</a>
   </nav>
 </header>
 
