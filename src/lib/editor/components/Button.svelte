1a50a188ac683eff0daba45b184fa45f84a35ce5
Icemourne <cboweris@gmail.com>
2024-06-22 18:08:33 +0300

    fixed active button state

--- src/lib/editor/components/Button.svelte
+++ src/lib/editor/components/Button.svelte
@@ -17,11 +17,9 @@ grid grid-cols-[1fr_3fr] justify-items-center items-center
 bg-LM-15 dark:bg-DM-15
 hover:bg-LM-25 hover:dark:bg-DM-25
 `
-
-// flex flex-row flex-nowrap gap-1 items-center
 </script>
 
-<button class:active {...props} class="{style}  {className ?? ''}">
+<button class:text-DM-accent={active} {...props} class="{style} {className ?? ''}">
   {@render children()}
 </button>
 
